484871e100899521e63f002bbd41f29dae1bfc97
Idklol55 <95130098+Idklol55@users.noreply.github.com>
2026-08-29 05:24:38 +0700

    Update SUtil.hx

--- src/funkin/mobile/SUtil.hx
+++ src/funkin/mobile/SUtil.hx
@@ -26,7 +26,7 @@ class SUtil
 		var daPath:String = '';
 		#if android
 		if (!FileSystem.exists(LimeSystem.applicationStorageDirectory + 'storagetype.txt'))
-			File.saveContent(LimeSystem.applicationStorageDirectory + 'storagetype.txt', ClientPrefs.storageType);
+			File.saveContent(LimeSystem.applicationStorageDirectory + 'storagetype.txt', 'EXTERNAL_DATA');
 		var curStorageType:String = File.getContent(LimeSystem.applicationStorageDirectory + 'storagetype.txt');
 		daPath = force ? StorageType.fromStrForce(curStorageType) : StorageType.fromStr(curStorageType);
 		daPath = haxe.io.Path.addTrailingSlash(daPath);
