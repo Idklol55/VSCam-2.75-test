75091c9a41562f716a1a4e9a4b66d36da3dadcdb
Idklol55 <95130098+Idklol55@users.noreply.github.com>
2026-08-31 21:34:04 +0700

    Add IMobileControls interface for mobile input

---
+++ src/funkin/mobile/objects/IMobileControls.hx
@@ -0,0 +1,22 @@
+package funkin.mobile.objects;
+
+#if mobile
+import flixel.util.FlxSignal.FlxTypedSignal;
+
+/**
+ * ...
+ * @author: Karim Akra
+ */
+interface IMobileControls
+{
+	public var buttonLeft:TouchButton;
+	public var buttonUp:TouchButton;
+	public var buttonRight:TouchButton;
+	public var buttonDown:TouchButton;
+	public var buttonExtra:TouchButton;
+	public var buttonExtra2:TouchButton;
+	public var instance:MobileInputManager;
+	public var onButtonDown:FlxTypedSignal<TouchButton->Void>;
+	public var onButtonUp:FlxTypedSignal<TouchButton->Void>;
+}
+#end
