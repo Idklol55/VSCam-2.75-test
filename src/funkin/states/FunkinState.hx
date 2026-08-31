d2976bbe5f1774b1f7960a876fb4d8a727715f3d
Idklol55 <95130098+Idklol55@users.noreply.github.com>
2026-08-31 22:34:37 +0700

    Update FunkinState.hx

--- src/funkin/states/FunkinState.hx
+++ src/funkin/states/FunkinState.hx
@@ -3,226 +3,96 @@ package funkin.states;
 import funkin.backend.FlxAudioHandler;
 import flixel.FlxState;
 
+#if mobile
+import flixel.group.FlxGroup;
+import funkin.mobile.controls.MobileHitbox;
+import funkin.mobile.controls.MobileVirtualPad;
+#end
+
 class FunkinState extends FlxState {
 	public static var menuSong:String = "";
 	var skipMusicCheck:Bool = false;
 
+	// using for mobile
+	public static var instance:FunkinState;
+
 	#if mobile
-	public var touchPad:TouchPad;
-	public var touchPadCam:FlxCamera;
-	public var luaTouchPad:TouchPad;
-	public var luaTouchPadCam:FlxCamera;
-	public var mobileControls:IMobileControls;
-	public var mobileControlsCam:FlxCamera;
+	public var hitbox:MobileHitbox;
+	public var virtualPad:MobileVirtualPad;
 
-	public function addTouchPad(DPad:String, Action:String)
+	public var virtualPadCam:FlxCamera;
+	public var hitboxCam:FlxCamera;
+
+    public function addVirtualPad(DPad:MobileDPadMode, Action:MobileActionMode)
 	{
-		touchPad = new TouchPad(DPad, Action);
-		add(touchPad);
+		virtualPad = new MobileVirtualPad(DPad, Action);
+		add(virtualPad);
 	}
-
-	public function removeTouchPad()
+	
+	public function addVirtualPadCamera(DefaultDrawTarget:Bool = false)
 	{
-		if (touchPad != null)
+		if (virtualPad != null)
 		{
-			remove(touchPad);
-			touchPad = FlxDestroyUtil.destroy(touchPad);
-		}
-
-		if (touchPadCam != null)
-		{
-			FlxG.cameras.remove(touchPadCam);
-			touchPadCam = FlxDestroyUtil.destroy(touchPadCam);
+			virtualPadCam = new FlxCamera();
+			virtualPadCam.bgColor.alpha = 0;
+			FlxG.cameras.add(virtualPadCam, DefaultDrawTarget);
+			
+			virtualPad.cameras = [virtualPadCam];
 		}
 	}
 
-	public function addMobileControls(defaultDrawTarget:Bool = false):Void
+	public function removeVirtualPad()
 	{
-		var extraMode = MobileData.extraActions.get(ClientPrefs.data.extraButtons);
-
-		switch (MobileData.mode)
+		if (virtualPad != null)
 		{
-			case 0: // RIGHT_FULL
-				mobileControls = new TouchPad('RIGHT_FULL', 'NONE', extraMode);
-			case 1: // LEFT_FULL
-				mobileControls = new TouchPad('LEFT_FULL', 'NONE', extraMode);
-			case 2: // CUSTOM
-				mobileControls = MobileData.getTouchPadCustom(new TouchPad('RIGHT_FULL', 'NONE', extraMode));
-			case 3: // HITBOX
-				mobileControls = new Hitbox(extraMode);
+			remove(virtualPad);
+			virtualPad = FlxDestroyUtil.destroy(virtualPad);
 		}
 
-		mobileControls.instance = MobileData.setButtonsColors(mobileControls.instance);
-		mobileControlsCam = new FlxCamera();
-		mobileControlsCam.bgColor.alpha = 0;
-		FlxG.cameras.add(mobileControlsCam, defaultDrawTarget);
+		if(virtualPadCam != null)
+		{
+			FlxG.cameras.remove(virtualPadCam);
+			virtualPadCam = FlxDestroyUtil.destroy(virtualPadCam);
+		}
+	}
 
-		mobileControls.instance.cameras = [mobileControlsCam];
-		mobileControls.instance.visible = false;
-		add(mobileControls.instance);
+	public function addMobileControls(DefaultDrawTarget:Bool = false)
+	{
+		hitbox = new MobileHitbox();
+
+		hitboxCam = new FlxCamera();
+		hitboxCam.bgColor.alpha = 0;
+		FlxG.cameras.add(hitboxCam, DefaultDrawTarget);
+
+		hitbox.cameras = [hitboxCam];
+		hitbox.visible = false;
+		add(hitbox);
 	}
 
 	public function removeMobileControls()
 	{
-		if (mobileControls != null)
+		if (hitbox != null)
 		{
-			remove(mobileControls.instance);
-			mobileControls.instance = FlxDestroyUtil.destroy(mobileControls.instance);
-			mobileControls = null;
+			remove(hitbox);
+			hitbox = FlxDestroyUtil.destroy(hitbox);
 		}
 
-		if (mobileControlsCam != null)
+		if(hitboxCam != null)
 		{
-			FlxG.cameras.remove(mobileControlsCam);
-			mobileControlsCam = FlxDestroyUtil.destroy(mobileControlsCam);
+			FlxG.cameras.remove(hitboxCam);
+			hitboxCam = FlxDestroyUtil.destroy(hitboxCam);
 		}
 	}
-
-	public function addTouchPadCamera(defaultDrawTarget:Bool = false):Void
-	{
-		if (touchPad != null)
-		{
-			touchPadCam = new FlxCamera();
-			touchPadCam.bgColor.alpha = 0;
-			FlxG.cameras.add(touchPadCam, defaultDrawTarget);
-			touchPad.cameras = [touchPadCam];
-		}
-	}
-/*
-	public function makeLuaTouchPad(DPadMode:String, ActionMode:String)
-	{
-		if (members.contains(luaTouchPad))
-			return;
-
-		if (!variables.exists("luaTouchPad"))
-			variables.set("luaTouchPad", luaTouchPad);
-
-		luaTouchPad = new TouchPad(DPadMode, ActionMode, NONE);
-		luaTouchPad.alpha = ClientPrefs.data.controlsAlpha;
-	}
-
-	public function addLuaTouchPad()
-	{
-		if (luaTouchPad == null || members.contains(luaTouchPad))
-			return;
-
-		var target = LuaUtils.getTargetInstance();
-		target.insert(target.members.length + 1, luaTouchPad);
-	}
-
-	public function addLuaTouchPadCamera(defaultDrawTarget:Bool = false)
-	{
-		if (luaTouchPad != null)
-		{
-			luaTouchPadCam = new FlxCamera();
-			luaTouchPadCam.bgColor.alpha = 0;
-			FlxG.cameras.add(luaTouchPadCam, defaultDrawTarget);
-			luaTouchPad.cameras = [luaTouchPadCam];
-		}
-	}
-
-	public function removeLuaTouchPad()
-	{
-		if (luaTouchPad != null)
-		{
-			luaTouchPad.kill();
-			luaTouchPad.destroy();
-			remove(luaTouchPad);
-			luaTouchPad = null;
-		}
-	}
-
-	public function luaTouchPadPressed(button:Dynamic):Bool
-	{
-		if (luaTouchPad != null)
-		{
-			if (Std.isOfType(button, String))
-				return luaTouchPad.buttonPressed(MobileInputID.fromString(button));
-			else if (Std.isOfType(button, Array))
-			{
-				var FUCK:Array<String> = button; // haxe said "You Can't Iterate On A Dyanmic Value Please Specificy Iterator or Iterable *insert nerd emoji*" so that's the only i found to fix
-				var idArray:Array<MobileInputID> = [];
-				for (strId in FUCK)
-					idArray.push(MobileInputID.fromString(strId));
-				return luaTouchPad.anyPressed(idArray);
-			}
-			else
-				return false;
-		}
-		return false;
-	}
-
-	public function luaTouchPadJustPressed(button:Dynamic):Bool
-	{
-		if (luaTouchPad != null)
-		{
-			if (Std.isOfType(button, String))
-				return luaTouchPad.buttonJustPressed(MobileInputID.fromString(button));
-			else if (Std.isOfType(button, Array))
-			{
-				var FUCK:Array<String> = button;
-				var idArray:Array<MobileInputID> = [];
-				for (strId in FUCK)
-					idArray.push(MobileInputID.fromString(strId));
-				return luaTouchPad.anyJustPressed(idArray);
-			}
-			else
-				return false;
-		}
-		return false;
-	}
-
-	public function luaTouchPadJustReleased(button:Dynamic):Bool
-	{
-		if (luaTouchPad != null)
-		{
-			if (Std.isOfType(button, String))
-				return luaTouchPad.buttonJustReleased(MobileInputID.fromString(button));
-			else if (Std.isOfType(button, Array))
-			{
-				var FUCK:Array<String> = button;
-				var idArray:Array<MobileInputID> = [];
-				for (strId in FUCK)
-					idArray.push(MobileInputID.fromString(strId));
-				return luaTouchPad.anyJustReleased(idArray);
-			}
-			else
-				return false;
-		}
-		return false;
-	}
-
-	public function luaTouchPadReleased(button:Dynamic):Bool
-	{
-		if (luaTouchPad != null)
-		{
-			if (Std.isOfType(button, String))
-				return luaTouchPad.buttonJustReleased(MobileInputID.fromString(button));
-			else if (Std.isOfType(button, Array))
-			{
-				var FUCK:Array<String> = button;
-				var idArray:Array<MobileInputID> = [];
-				for (strId in FUCK)
-					idArray.push(MobileInputID.fromString(strId));
-				return luaTouchPad.anyReleased(idArray);
-			}
-			else
-				return false;
-		}
-		return false;
-	}
-*/
 	#end
 
 	override function destroy()
 	{
+		super.destroy();
+
 		#if mobile
-		removeTouchPad();
-		// removeLuaTouchPad();
+		removeVirtualPad();
 		removeMobileControls();
 		#end
-
-		super.destroy();
 	}
 
 	override function create() {
